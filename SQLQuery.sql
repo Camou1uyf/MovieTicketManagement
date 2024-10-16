40712433c9410c680bfa3eb4caa0b9117fbc6556
Benboussad Lamara <107220517+Camou1uyf@users.noreply.github.com>
2024-10-16 16:26:51 +0100

    sql

---
+++ SQLQuery.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE movies1(
+id INT PRIMARY KEY IDENTITY(1,1),
+movie_id VARCHAR(MAX) NULL,
+movie_name VARCHAR(MAX) NULL,
+genre VARCHAR(MAX) NULL,
+price FLOAT NULL,
+capacity INT NULL,
+movie_image VARCHAR(MAX) NULL,
+status1 VARCHAR(MAX) NULL,
+created_at  DATETIME NULL,
+update_date DATE NULL,
+delete_date DATE NULL
+)
+
+SELECT*FROM movies1;
+
+DROP TABLE movies;
+ 
---
+++ sql.sql
@@ -0,0 +1,43 @@
+﻿SELECT*FROM movies1;
+
+CREATE TABLE cutomers(
+id int primary key identity(1,1),
+movie_id VARCHAR(MAX) NULL,
+seat_id VARCHAR(MAX) NULL,
+available_movie VARCHAR(MAX) NULL,
+TOTALPRICE fLOAT,
+foods VARCHAR(MAX) NULL,
+drinks VARCHAR(MAX) NULL,
+amount FLOAT,
+change1 FLOAT,
+status1 VARCHAR(MAX) NULL,
+date_buy VARCHAR(MAX) NULL
+
+)
+ 
+
+CREATE TABLE butticket(
+id int primary key identity(1,1),
+movie_id VARCHAR(MAX) NULL,
+seat_id VARCHAR(MAX) NULL,
+seat_number  int,
+price float null,
+status1 VARCHAR(MAX) NULL,
+created_at DATE
+) 
+
+SELECT* FROM butticket;
+
+SELECT capacity 
+FROM movies1 
+WHERE delete_date IS NULL 
+AND status1 != ' Delete';
+
+SELECT* FROM movies1;
+
+
+ drop table butticket;
+ 
+ SELECT COUNT(id) FROM movies1 WHERE status1 = 'Vailabale';
+
+ SELECT* FROM users;
